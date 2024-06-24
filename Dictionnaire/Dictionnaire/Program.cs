b569b14287b98b43cab9de56a3c7caa62443d175
remi1040 <48013030+remi1040@users.noreply.github.com>
2024-06-24 11:40:34 +0200

    Add files via upload

---
+++ Dictionnaire/Dictionnaire/Program.cs
@@ -0,0 +1,16 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        Dictionary<char,string> dic = new Dictionary<char,string>();
+
+        dic['a'] = "abruti";
+        dic['b'] = "boulot";
+        dic['c'] = "carrée";
+
+        dic.Remove('c');
+
+        foreach (string c in dic.Values) { 
+        Console.WriteLine(c);}
+    }
+}
